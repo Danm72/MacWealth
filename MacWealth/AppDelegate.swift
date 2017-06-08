0cb63cc3d535082a23c8e9ad4101bf4a6273ad13
Dan Malone <dan@mawla.io>
2017-06-08 17:11:32 +0100

    Initial Commit

---
+++ MacWealth/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacWealth
+//
+//  Created by Dan Malone on 08/06/2017.
+//  Copyright © 2017 Mawla. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
